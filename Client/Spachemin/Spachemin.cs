38bda9695a4650cafee2a8593f21b05f72e6a0d4
mcl <meowclewizlon@gmail.com>
2025-11-01 21:38:28 -0700

    Save

--- Client/Spachemin/Spachemin.cs
+++ Client/Spachemin/Spachemin.cs
@@ -1,5 +1,6 @@
 using System.Net.Sockets;
 using System.Text;
+using System.Diagnostics;
 
 namespace Spachemin;
 
@@ -7,8 +8,9 @@ public class Spachemin
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        string ip = Console.ReadLine(); // 44.245.211.131
+        Console.WriteLine("Hello, Spachemin!");
+        string? ip = Console.ReadLine(); // 44.245.211.131
+        if (ip == null) ip = "";
         if (ip.Length == 0) ip = "127.0.0.1";
         TcpClient client = new TcpClient();
         Console.WriteLine("Connecting to `" + ip + "`...");
@@ -23,17 +25,21 @@ public class Spachemin
         Console.WriteLine("Connected");
         while (true)
         {
-            String input = Console.ReadLine();
+            string? input = Console.ReadLine();
+            if (input == null) input = "";
             if (input.Length == 0) break;
             ASCIIEncoding encoder = new ASCIIEncoding();
             byte[] transData = encoder.GetBytes(input);
             Console.WriteLine("Sending...");
+            Stopwatch watch = Stopwatch.StartNew();
             _stream.Write(transData, 0, transData.Length);
             byte[] ackData = new byte[255];
             int ackLen = _stream.Read(ackData, 0, 255);
+            watch.Stop();
+            long latency = watch.ElapsedMilliseconds;
             string ackStr = "";
             for (int i = 0; i < ackLen; i++) ackStr += Convert.ToChar(ackData[i]);
-            Console.WriteLine("ACK: " + ackStr);
+            Console.WriteLine("ACK: " + ackStr + " (" + latency + ")");
         }
     }
 }
