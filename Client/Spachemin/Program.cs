4a9580accb5e2ef73f80053eca04ec5211d7d6eb
mcl <meowclewizlon@gmail.com>
2025-11-09 19:54:25 -0800

    Add Local Only Option

--- Client/Spachemin/Program.cs
+++ Client/Spachemin/Program.cs
@@ -9,6 +9,7 @@ public class Program
         Console.WriteLine("*** Spachemin ***");
         string? ip = null;
         if (!(args.Length > 0 && args[0] == "local")) ip = Console.ReadLine();
+        if (ip?.Trim().ToLower() == "l") ip = null;
         SpacheNetClient net = new SpacheNetClient();
         net.Connect(ip);
         Console.WriteLine("Connected");
